f28e3fef229e11895a870fade9cd7fab36b09045
mirotvorka <fordifferentusageso@gmail.com>
2026-03-06 15:12:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function shortenUrl(url) {
     let clean = url.replace(/^https?:\/\//, "");
     clean = clean.replace(/^www\./, "");
+
+    if (clean.startsWith("vk.com/")) {
+        let part = clean.split("/")[1];
+
+        if (!part) return "vk.com";
+
+        part = part.split("?")[0];
+        return part;
+    }
+
     return clean.split("/")[0];
 }
 
@@ -33,11 +43,19 @@ function generate() {
     const dateValue = dateInput.value || (newsMode ? "ЗАГОЛОВОК" : "ДД.ММ.ГГ");
 
     let news = document.getElementById("news").value || "Текст.";
-    const stream = document.getElementById("stream").value || "Текст.";
+    let stream = document.getElementById("stream").value || "Текст.";
+
     news = news.replace(/●/g, "•");
+    news = news.replace(/○/g, "∘");
+
+    let authorId = (document.getElementById("authorId")?.value || "").trim();
+    if (!authorId) authorId = "ID";
 
     let casterId = (document.getElementById("casterId")?.value || "").trim();
     if (!casterId) casterId = "ID";
+    news = news.replace(/catwar\.(?:net|su)\/blog(\d+)/gi, (match, id) => {
+        return `[url=blog${id}]блог[/url]`;
+    });
 
     const linkRegex = /(https?:\/\/\S+|[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\/\S*)/g;
 
@@ -52,9 +70,9 @@ function generate() {
     let template;
 
     if (!newsMode) {
-        template = `[font=cambria][size=13][color=#604a2e][bgrf=#83643f][pad=4 3 4 4][justify][divr=https://sun9-87.userapi.com/s/v1/ig2/tOsNZHat5ghBMLYe-vNRJ__70U1T-2KM1-i4inSw5g_G9jDSDN3Pr_UeG4bbDdz1pTeV58a7ybegJZ38piEKBTmV.jpg?quality=95&as=32x20,48x30,72x45,108x67,160x100,240x150,360x225,480x300,540x337,640x400,720x450,1080x675,1280x800,1440x900,1680x1050&from=bu&cs=1680x0][bgrf=#b08e6dBB][center][font=georgia][b][size=16][color=#4a3c2b][pad=8]СОБРАНИЕ, ${dateValue}[/pad][/color][/size][/b][/font][/center][/bgrf][bgrf=#83643f][pad=1.5][/pad][/bgrf][pad=20][bgrf=#c7a67f77][pad=3][font=georgia][b][size=14][color=#4a3c2b] НОВОСТИ[/color][/size][/b][/font][/pad][/bgrf][br]${news}[br][br][bgrf=#c7a67f44][pad=3][font=georgia][b][size=14][color=#4a3c2b][center][ [header=блок1]РАСКРЫТЬ ТРАНСЛЯЦИЮ СОБРАНИЯ[/header] ][/center][/color][/size][/b][/font][/pad][/bgrf][block=блок1][br]${stream}[br][size=9]Транслировал: [cat${casterId}].[/size][/block][/pad][/divr][/justify][/pad][/bgrf][/color][/size][/font]`;
+        template = `[font=cambria][size=13][color=#604a2e][bgrf=#83643f][pad=4 3 4 4][justify][divr=https://sun9-87.userapi.com/s/v1/ig2/tOsNZHat5ghBMLYe-vNRJ__70U1T-2KM1-i4inSw5g_G9jDSDN3Pr_UeG4bbDdz1pTeV58a7ybegJZ38piEKBTmV.jpg?quality=95&as=32x20,48x30,72x45,108x67,160x100,240x150,360x225,480x300,540x337,640x400,720x450,1080x675,1280x800,1440x900,1680x1050&from=bu&cs=1680x0][bgrf=#b08e6dBB][center][font=georgia][b][size=16][color=#4a3c2b][pad=8]СОБРАНИЕ, ${dateValue}[/pad][/color][/size][/b][/font][/center][/bgrf][bgrf=#83643f][pad=1.5][/pad][/bgrf][pad=20][bgrf=#c7a67f77][pad=3][font=georgia][b][size=14][color=#4a3c2b] НОВОСТИ[/color][/size][/b][/font][/pad][/bgrf][br]${news}[br][size=9]Пост составил: [cat${authorId}] [${authorId}][/size][br][br][bgrf=#c7a67f44][pad=3][font=georgia][b][size=14][color=#4a3c2b][center][ [header=блок1]РАСКРЫТЬ ТРАНСЛЯЦИЮ СОБРАНИЯ[/header] ][/center][/color][/size][/b][/font][/pad][/bgrf][block=блок1][br]${stream}[br][size=9]Транслировал собрание: [cat${casterId}] [${casterId}][/size][/block][/pad][/divr][/justify][/pad][/bgrf][/color][/size][/font]`;
     } else {
-        template = `[font=cambria][size=13][color=#604a2e][bgrf=#83643f][pad=4 3 4 4][justify][divr=https://sun9-87.userapi.com/s/v1/ig2/tOsNZHat5ghBMLYe-vNRJ__70U1T-2KM1-i4inSw5g_G9jDSDN3Pr_UeG4bbDdz1pTeV58a7ybegJZ38piEKBTmV.jpg?quality=95&as=32x20,48x30,72x45,108x67,160x100,240x150,360x225,480x300,540x337,640x400,720x450,1080x675,1280x800,1440x900,1680x1050&from=bu&cs=1680x0][bgrf=#b08e6dBB][center][font=georgia][b][size=16][color=#4a3c2b][pad=8]${dateValue}[/pad][/color][/size][/b][/font][/center][/bgrf][bgrf=#83643f][pad=1.5][/pad][/bgrf][pad=20][bgrf=#c7a67f77][pad=3][font=georgia][b][size=14][color=#4a3c2b] НОВОСТИ[/color][/size][/b][/font][/pad][/bgrf][br]${news}[br][/pad][/divr][/justify][/pad][/bgrf][/color][/size][/font]`;
+        template = `[font=cambria][size=13][color=#604a2e][bgrf=#83643f][pad=4 3 4 4][justify][divr=https://sun9-87.userapi.com/s/v1/ig2/tOsNZHat5ghBMLYe-vNRJ__70U1T-2KM1-i4inSw5g_G9jDSDN3Pr_UeG4bbDdz1pTeV58a7ybegJZ38piEKBTmV.jpg?quality=95&as=32x20,48x30,72x45,108x67,160x100,240x150,360x225,480x300,540x337,640x400,720x450,1080x675,1280x800,1440x900,1680x1050&from=bu&cs=1680x0][bgrf=#b08e6dBB][center][font=georgia][b][size=16][color=#4a3c2b][pad=8]${dateValue}[/pad][/color][/size][/b][/font][/center][/bgrf][bgrf=#83643f][pad=1.5][/pad][/bgrf][pad=20][bgrf=#c7a67f77][pad=3][font=georgia][b][size=14][color=#4a3c2b] НОВОСТИ[/color][/size][/b][/font][/pad][/bgrf][br]${news}[br][size=9]Пост составил: [cat${authorId}] [${authorId}][/size][/pad][/divr][/justify][/pad][/bgrf][/color][/size][/font]`;
     }
 
     document.getElementById("result").value = template;
@@ -64,11 +82,11 @@ function copyText() {
     const result = document.getElementById("result");
     result.select();
     result.setSelectionRange(0, 99999);
-    document.execCommand("copy");}
+    document.execCommand("copy");
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.getElementById("newsMode");
     if (toggle) toggle.addEventListener("change", updateModeUI);
     updateModeUI();
 });
-
